db1c4d18a0cc3f0751019d3ac770e4e366cd97a6
igiboi <36388765+igiboi@users.noreply.github.com>
2020-12-08 11:43:03 -0500

    added the latest repos

--- github.js
+++ github.js
@@ -2,15 +2,24 @@ class Github {
   constructor() {
     this.client_id = '38a6ea0c4d2e30d0e04e';
     this.client_secret = 'd160a517e7cf03deaa646867013a9f95ee6e5dea';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
+    // Github Profile url 
     const profileResponse = await fetch(`https://api.github.com/users/${user}?clied_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    // Repos url 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&clied_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json(); 
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      // similar repos: repos
+      repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -30,6 +30,33 @@ class UI {
   <div id="repos"></div>
     `;
   }
+
+  // Show user repos
+  showRepos(repos) {
+    let output = ''; 
+
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars:${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers:${repo.watchers_count}</span>
+            <span class="badge badge-success">Followers:${repo.forms_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    })
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+
+  }
+
   // an Alert message that will show when user is not found
   showAlert(message, className){
     // Clear any remaining alerts
